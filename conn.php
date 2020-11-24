d9eee91227603d20d04283e961dbbddb40487465
huffkinson <kvbt2002@gmail.com>
2020-11-24 11:30:02 +0800

    added conn.php file and modified index.php

---
+++ conn.php
@@ -0,0 +1,5 @@
+<?php
+	// $connection = new mysqli('localhost','boteacts-client','TLKTb!QN8t1}','boteacts_db') or die();
+	// $connection = new mysqli('localhost','id12256582_bote','bote2020','id12256582_bote') or die();
+	$connection = new mysqli('localhost','root','root','billing') or die(mysqli_error());
+?>
--- index.php
+++ index.php
@@ -1,4 +1,10 @@
 <!DOCTYPE html>
+<?php
+	// $connection = new mysqli('localhost','boteacts-client','TLKTb!QN8t1}','boteacts_db') or die();
+	// $connection = new mysqli('localhost','id12256582_bote','bote2020','id12256582_bote') or die();
+	$connection = new mysqli('localhost','root','root','billing') or die(mysqli_error());
+?>
+
 <html>
 <head>
 	<title>Bote Accounting and Taxation Services</title>
