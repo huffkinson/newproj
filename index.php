6624991bb060a71bf7e33aaac2198ce169dbfc14
huffkinson <kvbt2002@gmail.com>
2020-11-25 13:25:14 +0800

    updated login

--- index.php
+++ index.php
@@ -1,17 +1,19 @@
 <!DOCTYPE html>
-
 <html>
 <head>
-	<title>Bote Accounting and Taxation Services</title>
-	
+	<title>Main Page</title>
 	<meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
 	<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
 	<link rel="stylesheet" type="text/css" href="style.css">
+	<link rel="stylesheet" type="text/css" href="landing-page.min.css">
+	<script src="jquery/jquery.min.js"></script>
+	<script src="js/popper.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+
 </head>
 <body>
-
+	
 	<!-- Navigation starts here -->
 	<nav class="navbar navbar-expand-md navbar-light bg-light sticky-top">
 		<a class="navbar-brand" href="#">Bote Accounting and Taxation Services</a>
@@ -26,9 +28,9 @@
 				<li class="nav-item">
 					<a class="nav-link" href="#">About Us</a>
 				</li>
-				<li class="nav-item">
-					<a class="nav-link" href="#">Contact Us</a>
-				</li>
+				<!-- <li class="nav-item">
+					<a class="nav-link" href="#">Location</a>
+				</li> -->
 				<!--
 				<li class="nav-item dropdown">
 					<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown">Services</a>
@@ -38,7 +40,9 @@
 						<a class="dropdown-item" href="#">Portfolio</a>
 					</div>
 				</li> -->
-
+				<li class="nav-item">
+					<a class="nav-link" href="#">Contact Us</a>
+				</li>
 				<li class="nav-item">
 					<a href="login.php"><button type="button" class="btn btn-md btn-outline-secondary"> Login</button></a>
 				</li>
@@ -46,12 +50,82 @@
 		</div>
 	</nav>
 	<!-- Navigation ends here -->
-	<div class="container">
-		Under construction.
-	</div>
 
-	<script src="jquery/jquery.min.js"></script>
-	<script src="js/popper.min.js"></script>
-	<script src="js/bootstrap.min.js"></script>
+	<!-- Masthead starts here -->
+  <header class="masthead text-white text-center">
+    <div class="overlay"></div>
+    <div class="container">
+      <div class="row">
+        <div class="col-xl-9 mx-auto">
+          <h1>Bote Accounting and Taxation Services</h1>
+          <p style="font-size: 24px;">Providing our clients with excellent, innovative, and enterprising solutions</p>
+        </div>
+      </div>
+    </div>
+  </header>
+  <!-- Masthead ends here -->
+
+  <!-- Three Column Section -->
+	<div class="container-fluid padding">
+		<hr class="my-4"></hr>
+		<div class="row text-center padding">
+			<div class="col-xs-12 col-sm-6 col-md-3">
+				<img src="img/certified.png" class="my-icon">
+				<h3>Certified Public Accountants</h3>
+			</div>
+			<div class="col-xs-12 col-sm-6 col-md-3">
+				<img src="img/certified.png" class="my-icon">
+				<h3>Certified Internal Auditors</h3>
+			</div>
+			<div class="col-xs-12 col-sm-6 col-md-3">
+				<img src="img/certified.png" class="my-icon">
+				<h3>BOA Accredited</h3>
+			</div>
+			<div class="col-xs-12 col-sm-6 col-md-3">
+				<img src="img/certified.png" class="my-icon">
+				<h3>BIR Accredited</h3>
+			</div>
+		</div>
+		<hr class="my-4"></hr>
+	</div>
+  <!-- Footer -->
+	<footer>
+		<div class="container-fluid padding">
+			<div class="row text-center">
+				<div class="col-md-4">
+					<hr class="light"></hr>
+					<h5>Contact Info</h5>
+					<hr class="light"></hr>
+					<p>417-4292 / 417-6328 / 794-5332</p>
+					<p>boteacctgandtaxationservices@gmail.com</p>
+					
+				</div>
+				
+				<div class="col-md-4">
+					<hr class="light"></hr>
+					<h5>Office Address</h5>
+					<hr class="light"></hr>
+					<p>1052 Dela Cruz Compound Interior</p>
+					<p>Quirino Highway, Brgy. Sta. Monica</p>
+					<p>Novaliches, Quezon City</p>
+				</div>
+				<div class="col-md-4">
+					<hr class="light"></hr>
+					<h5>Office Hours</h5>
+					<hr class="light"></hr>
+					<p>Monday to Friday</p>
+					<p>8am - 5pm (PST)</p>
+				</div>
+				
+				<div class="col-12">
+					<hr class="light-100"></hr>
+					<h5>&copy; Bote Accounting and Taxation Services</h5>
+				</div>
+			</div>
+		</div>
+	</footer>
+
+
+
 </body>
 </html>
--- login.php
+++ login.php
@@ -2,33 +2,68 @@
 <html>
 <head>
 	<title>Login Page</title>
-
+	<meta charset="utf-8">
+    
 	<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="landing-page.min.css">
 	<link rel="stylesheet" type="text/css" href="style.css">
+
+	<style type="text/css">
+		
+		body{
+			background-color: white;
+		}
+
+		.login-form{
+			margin-top: 50px;
+			box-shadow: 0px 0px 10px 1px grey;
+			border-radius: 5px;
+			padding-bottom: 20px;
+			background: white;
+		}
+
+		.login-title{
+			background: #007bbf;
+			padding: 10px;
+			text-align: center;
+			color: #fff;
+			border-radius: 0px 0px 10px 10px;
+		}
+
+	</style>
+
 </head>
 <body>
-
-<div class="container">
-	<div class="container-fluid col-sm-6 col-md-6 col-lg-6">
-		<h2>Login</h2>
-		<form action="login2.php?op=in" method="POST">
-			<!-- username field -->
-			<div class="form-group">
-				<label>Username</label>
-				<input type="text" name="username" class="form-control">
-			</div>
-			<!-- password field -->
-			<div class="form-group">
-				<label>Password</label>
-				<input type="password" name="password" class="form-control" value="">
-			</div>
-			<!-- submit button -->
-			<div class="form-group">
-				<input type="submit" class="form-control btn btn-primary" value="LOGIN">
-				<a href="index.php" class="form-control btn btn-danger">Return to Main Page</a>
-			</div>
-		</form>
+	<div class="container">
+				<div class="login-form offset-md-4 col-sm-6 col-md-6 col-lg-4">
+					
+						<h2 class="login-title">Login</h2>
+					
+					<form action="login2.php?op=in" method="POST">
+						<!-- username field -->
+						<div class="form-group">
+							<label>Username</label>
+							<input type="text" name="username" class="form-control">
+						</div>
+						<!-- password field -->
+						<div class="form-group">
+							<label>Password</label>
+							<input type="password" name="password" class="form-control">
+						</div>
+						<!-- submit button -->
+						<div class="form-group">
+							<input type="submit" class="form-control btn btn-primary" value="LOGIN">
+						</div>
+						<div class="form-group col-lg-12">
+							<a href="index.php"><button type="button" class="btn btn-md btn-outline-danger">Return to Homepage</button></a>
+						</div>
+					</form>
+				</div>
 	</div>
-</div>
+	
+	<script src="jquery/jquery.min.js"></script>
+	<script src="js/popper.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/datatables.min.js"></script>
 </body>
 </html>
