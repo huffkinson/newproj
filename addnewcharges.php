1ee8d98a892ba820172af3ff84429f132a81b100
huffkinson <kvbt2002@gmail.com>
2020-12-02 20:05:26 +0800

    updated charges can add new charges now

--- addnewcharges.php
+++ addnewcharges.php
@@ -1,15 +1,26 @@
 <?php
 	include('conn.php');
+	
+	// remove when ready to upload to server
+	error_reporting(E_ALL);
+	ini_set('display_errors', 1);
+
 
 	
-	$name = $_POST['client_name'];
-	$description = $_POST['client_vat_type'];
-	$amount = $_POST['client_rdo'];
+	$name = $_POST['name'];
+	$description = $_POST['description'];
+	$amount = $_POST['amount'];
 	
 
-	$SQL = "INSERT INTO tbl_charges(name,description,amount) VALUES('$id','$name','$description','$amount')";
+	$SQL = "INSERT INTO tbl_charges(name,description,amount) VALUES('$name','$description','$amount')";
 
-	mysqli_query($connection, $SQL);
+	// mysqli_query($connection, $SQL);
+	$result = $connection->query($SQL);
+	if (!$result) {
+		// header('location: charges.php');
+   		printf("%s\n", $connection->error);
+   		exit();
+	}
 	
 	header('location: charges.php');
 ?>
